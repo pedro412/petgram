0ea376fa32d0ca084ee72c3645c5b972a7d265e4
pedro412 <pedro.alvarez412@outlook.com>
2019-07-19 12:05:51 -0500

    section 11

---
+++ src/components/ListOfCategories/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Item = styled.li`
+    padding: 0 8px;
+`
+
+export const List = styled.ul`
+    display: flex;
+    overflow: auto;
+    width: 100%
+`
